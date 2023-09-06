836d771b03bdc1a7f9685fd1b7a117cdb1334f48
root <ibm11@live.com>
2023-09-06 10:15:02 -0700

    UPDATE

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{:02d}".format(i), end=', ')
-print("{:02d}".format(i + 1))
+print(", ".join("{0:0>2}".format(i) for i in range(100)))
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 8):
+for i in range(10):
     for j in range(i + 1, 10):
-        print("{:d}{:d}".format(i, j), end=', ')
-print("{:d}{:d}".format(i + 1, j))
+        print("{}".format(str(i) + str(j)), end="")
+        if int(str(i) + str(j)) < 89:
+            print(", ", end="")
+print("")
