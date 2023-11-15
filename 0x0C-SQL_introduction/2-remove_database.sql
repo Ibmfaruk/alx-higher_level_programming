c9db0c7aa4207787e82e940ef55f9d5edc80f285
Ibrahim Muhammad Faruk <54984365+Ibmfaruk@users.noreply.github.com>
2023-11-15 07:05:49 +0100

    Delete 0x0C-SQL_introduction directory

--- 0x0C-SQL_introduction/2-remove_database.sql
+++
@@ -1,4 +0,0 @@
--- Delete the database 'hbtn_0c_0'
--- If 'hbtn_0c_0' doesn't exist, script should not fail
--- Not allowed to use SELECT or SHOW
-DROP DATABASE IF EXISTS hbtn_0c_0;
--- 0x0C-SQL_introduction/5-full_table.sql
+++
@@ -1,4 +0,0 @@
--- Print the full description of 'first_table' from db 'hbtn_0c_0'
--- db name will be passed as an arg to mysql cmd
--- Not allowed to use DESCRIBE or EXPLAIN
-SHOW CREATE TABLE first_table;
